fb7b710a46e870f7fa1df6a37c9577e5a924cf74
S H-SuHan <suhanur35-448@diu.edu.bd>
2024-05-05 17:50:00 +0600

    Inheritance of oop

---
+++ module3/3.2.ts
@@ -0,0 +1,43 @@
+class Person {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string){
+        this.name = name;
+        this.age = age;
+        this.address =address;
+    }
+
+    getSleep(numOfHours: number){
+        console.log(`${this.name} will sleep for ${numOfHours}`);
+    }
+}
+
+class Student extends Person {
+    constructor(name: string, age: number, address: string){
+        super(name, age, address)
+    }
+}
+
+const student = new Student("SuHan", 24, "Dhaka")
+// student.getSleep
+
+
+class Teacher extends Person{
+    gender: string;
+
+    constructor(name: string, age: number, address: string, gender: string){
+        super(name, age, address)
+        this.gender = gender;
+
+    }
+
+
+    takeClass (numOfClass : number){
+        console.log(`${this.name} will take ${numOfClass}`);
+    }
+}
+
+const teacher = new Teacher("SuHan", 24, "Dhaka" , "female")
+// teacher.takeClass
